efd606b9b5be1d45f1d31387a45e49386999abed
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-03 16:51:56 +0100

    ho finito l'esercizio

--- react-hello-world/src/App.jsx
+++ react-hello-world/src/App.jsx
@@ -1,9 +1,11 @@
+import viteLogo from './assets/img/react.svg'
+
 export default function App() {
   return (
     <>
       
       <h1>Hello Word</h1>
-      
+      <img src={viteLogo} className="logo" alt="Vite logo" />
        
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo numquam aliquam necessitatibus ab nihil optio placeat autem, saepe fugit consequuntur labore dolorem. Rerum necessitatibus voluptatem corrupti maxime nulla. Magnam, laudantium.
